6d7965d45d5b05f211ebbce60244e9d06f90135f
Furqan Ali <fali@fetchsky.com>
2019-05-19 17:03:02 +0500

    changes

--- android/src/main/java/com/staltz/reactnativeandroidlocalnotification/Notification.java
+++ android/src/main/java/com/staltz/reactnativeandroidlocalnotification/Notification.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import android.net.Uri;
 import android.app.NotificationChannel;
 
+import java.io.IOException;
 import java.lang.System;
 import java.net.URL;
 
